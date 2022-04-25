236766c57a2516150e11d06c8686742fc8ba6266
Ibrahim-Rezq <ovic391@gmail.com>
2022-04-25 14:26:28 +0200

    refactoring

--- js.js
+++ js.js
@@ -1,73 +1,92 @@
+const getData = async () => {
+    const res = await fetch(
+        'https://random-word-api.herokuapp.com/word?length=5'
+    )
+    const word = await res.json()
+
+    if (
+        word[0].split('').some((v, i, a) => {
+            return a.lastIndexOf(v) !== i
+        })
+    )
+        getData()
+    else Game.word = word[0]
+}
+getData()
+const letters = document.querySelectorAll('.letter')
+const keys = document.querySelectorAll('.key')
+
 const Game = {
     gusses: [],
-    word: [],
-    Theworde: 'print',
-    squreNum: 0,
-    AddSqure: function () {
-        this.squreNum += 1
+    currGusse: [],
+    word: 'print',
+    currBox: 0,
+    nextBox: function () {
+        this.currBox += 1
     },
-    RemoveSqure: function (letter) {
-        if (this.squreNum > 0) {
+    prevBox: function (letter) {
+        if (this.currBox > 0) {
             letter.textContent = ''
-            this.squreNum -= 1
-            this.word.pop()
+            this.currBox -= 1
+            this.currGusse.pop()
         }
     },
-    ResetSqure: function () {
-        if (this.gusses[this.gusses.length - 1] == this.Theworde) {
+    nextRow: function () {
+        if (this.gusses[this.gusses.length - 1] == this.word) {
             alert('youwin')
-        } else {
-            this.squreNum = 0
-            this.word = []
+        } else if (this.gusses.length >= 6) alert('Good luck next time')
+        else {
+            this.currBox = 0
+            this.currGusse = []
         }
     },
-    AddGusse: function () {
+    submitGusse: function () {
         this.gusses.push(
-            this.word
-                .map((e, i) => {
-                    if (this.Theworde.indexOf(e.letter) != -1) {
-                        if (this.Theworde.indexOf(e.letter) == i) {
-                            e.squre.style.background = 'green'
-                            e.elem.style.background = 'green'
+            this.currGusse
+                .map((gusse, i) => {
+                    setTimeout(() => {
+                        gusse.squre.className += ' go'
+                    }, 50 * i)
+                    if (this.word.indexOf(gusse.letter) != -1) {
+                        if (this.word.indexOf(gusse.letter) == i) {
+                            gusse.squre.style.background = 'hsl(125, 67%, 44%)'
+                            gusse.elem.style.background = 'hsl(125, 67%, 44%)'
                         } else {
-                            e.elem.style.background = 'red'
-                            e.squre.style.background = 'red'
+                            gusse.elem.style.background = 'hsl(13, 88%, 68%)'
+                            gusse.squre.style.background = 'hsl(13, 88%, 68%)'
                         }
                     }
-                    return e.letter
+                    return gusse.letter
                 })
                 .join('')
         )
     },
-    addToWord: function (val, elem, squre) {
-        return this.word.push({ letter: val, elem: elem, squre: squre })
+    addLetterToGusse: function (val, elem, squre) {
+        return this.currGusse.push({ letter: val, elem: elem, squre: squre })
     },
-    row: function () {
-        return this.squreNum + this.gusses.length * 5
+    getRow: function () {
+        return this.currBox + this.gusses.length * 5
     },
 }
-const letters = document.querySelectorAll('.letter')
-const keys = document.querySelectorAll('.key')
 const handelKeyClick = (e) => {
     const val = e.target.textContent
-    if (val === 'enter' && Game.squreNum % 5 === 0 && Game.squreNum !== 0) {
-        Game.AddGusse()
-        console.log(Game.gusses)
-        console.log(Game.gusses[Game.gusses.length - 1])
-        Game.ResetSqure()
-    } else if (val === 'del') Game.RemoveSqure(letters[Game.row() - 1])
+    if (val === 'enter' && Game.currBox % 5 === 0 && Game.currBox !== 0) {
+        Game.submitGusse()
+        Game.nextRow()
+    } else if (val === 'del') Game.prevBox(letters[Game.row() - 1])
     else {
         if (
-            (Game.squreNum % 5 !== 0 || Game.squreNum === 0) &&
+            (Game.currBox % 5 !== 0 || Game.currBox === 0) &&
             val !== 'enter' &&
             val !== 'del'
         ) {
-            letters[Game.row()].textContent = val.toUpperCase()
-            Game.AddSqure()
-            Game.addToWord(val, e.target, letters[Game.row() - 1])
+            letters[Game.getRow()].textContent = val.toUpperCase()
+            Game.nextBox()
+            Game.addLetterToGusse(val, e.target, letters[Game.getRow() - 1])
         }
     }
 }
+
 keys.forEach((key) => {
     key.addEventListener('click', handelKeyClick)
 })
