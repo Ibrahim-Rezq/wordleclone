17265c8d3531d70bfa46206190135d380a19699c
unknown <ovic391@gmail.com>
2022-04-25 22:06:38 +0200

    adding duplicate words failsafe , refactoring code

--- js.js
+++ js.js
@@ -1,92 +1,104 @@
-const getData = async () => {
-    const res = await fetch(
-        'https://random-word-api.herokuapp.com/word?length=5'
-    )
-    const word = await res.json()
+const letters = document.querySelectorAll('.letter');
+const keys = document.querySelectorAll('.key');
 
-    if (
-        word[0].split('').some((v, i, a) => {
-            return a.lastIndexOf(v) !== i
-        })
-    )
-        getData()
-    else Game.word = word[0]
-}
-getData()
-const letters = document.querySelectorAll('.letter')
-const keys = document.querySelectorAll('.key')
+const words = ['panda', 'super', 'words', 'islam', 'smile', 'party'];
 
-const Game = {
-    gusses: [],
-    currGusse: [],
-    word: 'print',
-    currBox: 0,
-    nextBox: function () {
-        this.currBox += 1
-    },
-    prevBox: function (letter) {
-        if (this.currBox > 0) {
-            letter.textContent = ''
-            this.currBox -= 1
-            this.currGusse.pop()
-        }
-    },
-    nextRow: function () {
-        if (this.gusses[this.gusses.length - 1] == this.word) {
-            alert('youwin')
-        } else if (this.gusses.length >= 6) alert('Good luck next time')
-        else {
-            this.currBox = 0
-            this.currGusse = []
-        }
-    },
-    submitGusse: function () {
-        this.gusses.push(
-            this.currGusse
-                .map((gusse, i) => {
-                    setTimeout(() => {
-                        gusse.squre.className += ' go'
-                    }, 50 * i)
-                    if (this.word.indexOf(gusse.letter) != -1) {
-                        if (this.word.indexOf(gusse.letter) == i) {
-                            gusse.squre.style.background = 'hsl(125, 67%, 44%)'
-                            gusse.elem.style.background = 'hsl(125, 67%, 44%)'
-                        } else {
-                            gusse.elem.style.background = 'hsl(13, 88%, 68%)'
-                            gusse.squre.style.background = 'hsl(13, 88%, 68%)'
-                        }
-                    }
-                    return gusse.letter
-                })
-                .join('')
-        )
-    },
-    addLetterToGusse: function (val, elem, squre) {
-        return this.currGusse.push({ letter: val, elem: elem, squre: squre })
-    },
-    getRow: function () {
-        return this.currBox + this.gusses.length * 5
-    },
-}
-const handelKeyClick = (e) => {
-    const val = e.target.textContent
-    if (val === 'enter' && Game.currBox % 5 === 0 && Game.currBox !== 0) {
-        Game.submitGusse()
-        Game.nextRow()
-    } else if (val === 'del') Game.prevBox(letters[Game.row() - 1])
-    else {
-        if (
-            (Game.currBox % 5 !== 0 || Game.currBox === 0) &&
-            val !== 'enter' &&
-            val !== 'del'
-        ) {
-            letters[Game.getRow()].textContent = val.toUpperCase()
-            Game.nextBox()
-            Game.addLetterToGusse(val, e.target, letters[Game.getRow() - 1])
-        }
+class Game {
+  constructor(word) {
+    this.gusses = [];
+    this.currGusse = [];
+    this.word = word;
+    this.currBox = 0;
+  }
+
+  nextBox = function () {
+    this.currBox += 1;
+  };
+  prevBox = function (letter) {
+    if (this.currBox > 0) {
+      letter.textContent = '';
+      this.currBox -= 1;
+      this.currGusse.pop();
     }
+  };
+  nextRow = function () {
+    if (this.gusses[this.gusses.length - 1] == this.word) {
+      alert('youwin');
+    } else if (this.gusses.length >= 6) alert('Good luck next time');
+    else {
+      this.currBox = 0;
+      this.currGusse = [];
+    }
+  };
+  submitGusse = function () {
+    if (
+      this.gusses.indexOf(
+        this.currGusse.map((letter) => letter.letter).join('')
+      ) < 0
+    ) {
+      this.gusses.push(
+        this.currGusse
+          .map((gusse, i) => {
+            setTimeout(() => {
+              gusse.squre.className += ' go';
+            }, 50 * i);
+            console.log(this.word);
+            if (this.word.indexOf(gusse.letter) != -1) {
+              if (this.word.indexOf(gusse.letter) == i) {
+                gusse.squre.style.background = 'hsl(125, 67%, 44%)';
+                gusse.elem.style.background = 'hsl(125, 67%, 44%)';
+              } else {
+                gusse.elem.style.background = 'hsl(13, 88%, 68%)';
+                gusse.squre.style.background = 'hsl(13, 88%, 68%)';
+              }
+            }
+            return gusse.letter;
+          })
+          .join('')
+      );
+    } else {
+      this.currGusse.map((gusse, i) => {
+        gusse.squre.textContent = '';
+      });
+      alert('you alredy enterd this');
+    }
+  };
+  addLetterToGusse = function (val, elem, squre) {
+    return this.currGusse.push({ letter: val, elem: elem, squre: squre });
+  };
+  getRow = function () {
+    return this.currBox + this.gusses.length * 5;
+  };
 }
+const getRandom = (max = 1, min = 0) => {
+  return Math.random() * (max - min) + min;
+};
+const getRandomInt = (max = 1, min = 0) => {
+  return Math.floor(getRandom(max, min));
+};
+
+const game = new Game(words[getRandomInt(words.length - 1)]);
+console.log(game.word);
+
+const handelKeyClick = (e) => {
+  const val = e.target.textContent;
+  if (val === 'enter' && game.currBox % 5 === 0 && game.currBox !== 0) {
+    game.submitGusse();
+    game.nextRow();
+  } else if (val === 'del') game.prevBox(letters[game.getRow() - 1]);
+  else {
+    if (
+      (game.currBox % 5 !== 0 || game.currBox === 0) &&
+      val !== 'enter' &&
+      val !== 'del'
+    ) {
+      letters[game.getRow()].textContent = val.toUpperCase();
+      game.nextBox();
+      game.addLetterToGusse(val, e.target, letters[game.getRow() - 1]);
+    }
+  }
+};
 
 keys.forEach((key) => {
-    key.addEventListener('click', handelKeyClick)
-})
+  key.addEventListener('click', handelKeyClick);
+});
